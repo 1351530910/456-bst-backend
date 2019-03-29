84dee82d32549b87760fc4f291b3fd0f06f27422
chaoyi liu <chaoyi97@gmail.com>
2019-03-29 00:12:29 -0400

    add bst methods

--- 456/Model/UserDB.cs
+++ 456/Model/UserDB.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace bst.Model
 {
@@ -14,6 +15,16 @@ namespace bst.Model
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                foreach (var property in entityType.GetProperties())
+                {
+                    if (property.ClrType == typeof(bool))
+                    {
+                        property.SetValueConverter(new BoolToIntConverter());
+                    }
+                }
+            }
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -36,6 +47,7 @@ namespace bst.Model
         public DbSet<Study> Studies { get; set; }
         public DbSet<Subject> Subjects { get; set; }
         public DbSet<History> Histories { get; set; }
+        public DbSet<FunctionalFile> FunctionalFiles { get; set; }
         public DbSet<Channel> Channels { get; set; }
         public DbSet<TimeFreq> TimeFreqs { get; set; }
         public DbSet<Stat> Stats { get; set; }
@@ -54,6 +66,22 @@ namespace bst.Model
         public DbSet<Surface> surfaces { get; set; }
         public DbSet<Session> sessions { get; set; }
     }
+
+
+    public class BoolToIntConverter : ValueConverter<bool, int>
+    {
+        public BoolToIntConverter(ConverterMappingHints mappingHints = null)
+            : base(
+                  v => Convert.ToInt32(v),
+                  v => Convert.ToBoolean(v),
+                  mappingHints)
+        {
+        }
+        public static ValueConverterInfo DefaultInfo { get; }
+            = new ValueConverterInfo(typeof(bool), typeof(int), i => new BoolToIntConverter(i.MappingHints));
+    }
+
+    #region user-group
     public class Session
     {
         [Key]
@@ -132,6 +160,10 @@ namespace bst.Model
 
         public string message { get; set; }
     }
+
+    #endregion
+
+
     public class Protocol
     {
         [Key]
@@ -144,23 +176,6 @@ namespace bst.Model
         public bool IsLocked { get; set; }
         
         public virtual User LockedUser { get; set; }
-    }
-
-    public class Study
-    {
-        [Key]
-        public Guid id { get; set; }
-        //metadata
-        public string Filename { get; set; }
-        public string Name { get; set; }
-        public string Condition { get; set; }
-        public DateTime DateOfStudy { get; set; }
-        public int IChannel { get; set; }
-        public int IHeadModel { get; set; }
-        
-        public virtual Protocol Protocol { get; set; }
-        
-        public virtual Subject Subject { get; set; }
     }
 
     public class Subject
@@ -183,6 +198,22 @@ namespace bst.Model
         public virtual Protocol Protocol { get; set; }
     }
 
+    public class Study
+    {
+        [Key]
+        public Guid id { get; set; }
+        //metadata
+        public string Filename { get; set; }
+        public string Name { get; set; }
+        public string Condition { get; set; }
+        public DateTime DateOfStudy { get; set; }
+        public int IChannel { get; set; }
+        public int IHeadModel { get; set; }
+
+        public virtual Protocol Protocol { get; set; }
+        public virtual Subject Subject { get; set; }
+    }
+
     public class History
     {
         [Key]
@@ -199,12 +230,6 @@ namespace bst.Model
 
     #region Functional File and its subclasses 
 
-    public enum FunctionalFileType
-    {
-        channel, timefreq, stat, headmodel, result, recording, matrix,
-        dipole, covariance, image
-    }
-
     public class FunctionalFile
     {
         [Key]
@@ -212,9 +237,9 @@ namespace bst.Model
         //metadata
         public string Comment { get; set; }
         public string FileName { get; set; }
-        public FunctionalFileType FileType { get; set; }
+        public string FileType { get; set; }
         
-        public Study Study { get; set; }
+        public virtual Study Study { get; set; }
 
     }
 
@@ -222,7 +247,6 @@ namespace bst.Model
     {
         [Key]
         public Guid id { get; set; }
-        //public Guid ChannelID { get; set; }
         //metadata
         public int NbChannels { get; set; }
         public string TransfMegLabels { get; set; }
@@ -367,11 +391,6 @@ namespace bst.Model
 
     #region Anatomical File and its subclasses 
 
-    public enum AnatomicalFileType
-    {
-        Fiber, Volume, Surface
-    }
-
     public class AnatomicalFile
     {
         [Key]
@@ -379,7 +398,7 @@ namespace bst.Model
         //metadata
         public string Comment { get; set; }
         public string FileName { get; set; }
-        public int FileType { get; set; }
+        public string FileType { get; set; }
         
         public virtual Subject Subject { get; set; }
     }
---
+++ 456/Model/bstPackage.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bst.Model
+{
+    public class ProtocolData
+    {
+        //metadata
+        public string Comment { get; set; }
+        public int IStudy { get; set; }
+        public bool UseDefaultAnat { get; set; }
+        public bool UseDefaultChannel { get; set; }
+        public bool IsLocked { get; set; }
+
+        public Guid LockedUserId { get; set; }
+    }
+
+    public class SubjectData
+    {
+        //metadata
+        public string Comment { get; set; }
+        public string Filename { get; set; }
+        public string Name { get; set; }
+        public bool UseDefaultAnat { get; set; }
+        public bool UseDefaultChannel { get; set; }
+        public int IAnatomy { get; set; }
+        public int IScalp { get; set; }
+        public int ICortex { get; set; }
+        public int IInnerSkull { get; set; }
+        public int IOuterSkull { get; set; }
+        public int IOther { get; set; }
+
+        public Guid ProtocolId { get; set; }
+    }
+
+    public class StudyData
+    {
+        //metadata
+        public string Filename { get; set; }
+        public string Name { get; set; }
+        public string Condition { get; set; }
+        public DateTime DateOfStudy { get; set; }
+        public int IChannel { get; set; }
+        public int IHeadModel { get; set; }
+
+        public Guid ProtocolId { get; set; }
+        public Guid SubjectId { get; set; }
+    }
+
+
+    public class HistoryData
+    {
+        //metadata
+        public DateTime TimeStamp { get; set; }
+        public string Type { get; set; }
+        public string HistoryEvent { get; set; }
+
+        public Guid FunctionalFileId { get; set; }
+        public Guid AnatomicalFileId { get; set; }
+    }
+
+    #region Functional File and its subclasses 
+
+
+    public class FunctionalFileData
+    {
+        //metadata
+        public string Comment { get; set; }
+        public string FileName { get; set; }
+        public string FileType { get; set; }
+
+        public Guid StudyId { get; set; }
+
+    }
+
+    public class ChannelData
+    {
+        //metadata
+        public int NbChannels { get; set; }
+        public string TransfMegLabels { get; set; }
+        public string TransfEegLabels { get; set; }
+        /*
+        //summary info
+        public bool DbMegRefCoef { get; set; }
+        public int DbProjector { get; set; }
+        public int DbHeadPoints { get; set; }
+        public int DbTransfMeg { get; set; }
+        public int DbTransfEeg { get; set; }
+        public int DbIntraElectrodes { get; set; }
+        */
+        public Guid ParentId { get; set; }
+    }
+
+    public class TimeFreqData
+    {
+        //metadata
+        public string Measure { get; set; }
+        public string Method { get; set; }
+        public int NAvg { get; set; }
+        public string ColormapType { get; set; }
+        public string DisplayUnits { get; set; }
+
+        //summary info 
+        public Guid ParentId { get; set; }
+
+    }
+
+    public class StatData
+    {
+        //metadata 
+        public int Df { get; set; }
+        public bool Correction { get; set; }
+        public string Type { get; set; }
+        /*
+        //summary info
+        public int DbChannelFlag { get; set; }  
+        */
+        public Guid ParentId { get; set; }
+    }
+
+    public class HeadModelData
+    {
+        //metadata
+        public string Type { get; set; }
+        public string MEGMethod { get; set; }
+        public string EEGMethod { get; set; }
+        public string ECOGMethod { get; set; }
+        public string SEEGMethod { get; set; }
+
+        //summary info
+        public Guid ParentId { get; set; }
+    }
+
+    public class ResultData
+    {
+        //metadata 
+        public bool IsLink { get; set; }
+        public int NComponents { get; set; }
+        public string Function { get; set; }
+        public int NAvg { get; set; }
+        public string ColormapType { get; set; }
+        public string DisplayUnits { get; set; }
+
+        /*
+        //summary info
+        public bool DbImagingKernel { get; set; }
+        public int DbChannelFlag { get; set; }
+        public int DbAtlas { get; set; }
+        */
+
+        public Guid ParentId { get; set; }
+    }
+
+    public class RecordingData
+    {
+        //metadata 
+        public string Format { get; set; }
+        public string Device { get; set; }
+        public char Byteorder { get; set; }
+        public string DataType { get; set; }
+        public int NAvg { get; set; }
+        public int SFreq { get; set; }
+        public double TimeStart { get; set; }
+        public double TimeEnd { get; set; }
+        public int SamplesStart { get; set; }
+        public int SamplesEnd { get; set; }
+        public int CurrCrfComp { get; set; }
+        public int DestCtfComp { get; set; }
+        public DateTime Acq_Date { get; set; }
+        public string ColormapType { get; set; }
+        public string DisplayUnits { get; set; }
+        public bool IsBids { get; set; }
+
+        /*
+        //summary info
+        public int DbEvents { get; set; }
+        public int DbEpoch { get; set; }
+        public int DbChannelFlag { get; set; }
+        */
+        public Guid ParentId { get; set; }
+    }
+
+    public class MatrixData
+    {
+        //metadata
+        public int NAvg { get; set; }
+        public string DisplayUnits { get; set; }
+        /*
+        //summary info
+        public int DbChannelFlag { get; set; }
+        public int DbEvents { get; set; }
+        public int DbAtlas { get; set; }
+        */
+        public Guid ParentId { get; set; }
+    }
+
+
+    /*
+
+    public class Dipole
+    {
+        [Key]
+        public Guid id { get; set; }
+        //summary info 
+        public int DbDipole { get; set; }
+        public virtual FunctionalFile parent { get; set; }
+    }
+
+    public class Covariance
+    {
+        [Key]
+        public Guid id { get; set; }
+        public virtual FunctionalFile parent { get; set; }
+    }
+
+    public class Image
+    {
+        [Key]
+        public Guid id { get; set; }
+        public virtual FunctionalFile parent { get; set; }
+    }
+    */
+    #endregion
+
+    #region Anatomical File and its subclasses 
+    /*
+    public enum AnatomicalFileType
+    {
+        Fiber, Volume, Surface
+    }
+
+    public class AnatomicalFile
+    {
+        [Key]
+        public Guid id { get; set; }
+        //metadata
+        public string Comment { get; set; }
+        public string FileName { get; set; }
+        public int FileType { get; set; }
+
+        public virtual Subject Subject { get; set; }
+    }
+
+    public class Fiber
+    {
+        [Key]
+        public Guid id { get; set; }
+
+        public virtual AnatomicalFile parent { get; set; }
+    }
+
+    public class Volume
+    {
+        [Key]
+        public Guid id { get; set; }
+        //summary info
+        public int DbCubeX { get; set; }
+        public int DbCubeY { get; set; }
+        public int DbCubeZ { get; set; }
+        public double DbVoxsizeX { get; set; }
+        public double DbVoxsizeY { get; set; }
+        public double DbVoxsizeZ { get; set; }
+        public bool DbSCS { get; set; }
+        public bool DbNCS { get; set; }
+
+        public virtual AnatomicalFile parent { get; set; }
+    }
+
+    public class Surface
+    {
+        [Key]
+        public Guid id { get; set; }
+        //metadata
+        public int IAtlas { get; set; }
+        //summary info
+        public int DbVertices { get; set; }
+        public int DbFaces { get; set; }
+        public int DbAtlas { get; set; }
+
+        public virtual AnatomicalFile parent { get; set; }
+    }
+    */
+    #endregion
+}
