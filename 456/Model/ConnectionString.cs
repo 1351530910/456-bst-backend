c883746519d5a1cef1eeccdcb9858f1215d6e012
1351530910 <chen.he2@mail.mcgill.ca>
2019-09-18 13:26:39 -0400

    Update ConnectionString.cs

--- 456/Model/ConnectionString.cs
+++ 456/Model/ConnectionString.cs
@@ -4,15 +4,7 @@ namespace bst.Model
 {
     public class ConnectionString
     {
-        public static string servertype = "MYSQL";
-        public static string connectionstring = "server=localhost;database=bstusers;user=bst;password=asd45214";
-        static ConnectionString()
-        {
-            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT").Equals("Development"))
-            {
-                servertype = "MSSQL";
-                connectionstring = "server=localhost;database=bstusers;user=sa;password=asd45214..";
-            }
-        }
+        public static string servertype = "";
+        public static string connectionstring = "";
     }
 }
