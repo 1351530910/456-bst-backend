81ca42f448590e96ef64de1be1c3672c6ea77485
Chen <chen.he2@mail.mcgill.ca>
2019-09-23 11:53:07 -0400

    connectionstring example

---
+++ 456/Model/ConnectionStringExample.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace bst.Model
+{
+    public class ConnectionStringExample
+    {
+        public static string servertype = "MYSQL";
+        public static string connectionstring = "server=localhost;database=bstusers;user=root;password=pwd";
+    }
+}
